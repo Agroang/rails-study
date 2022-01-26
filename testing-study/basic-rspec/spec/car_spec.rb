199b6a148c8461f539be25f8c3f2c1cb3315a60d
matias acuna <agroang@gmail.com>
2022-01-26 16:08:28 +0900

    added tests for the class method and instance method

--- testing-study/basic-rspec/spec/car_spec.rb
+++ testing-study/basic-rspec/spec/car_spec.rb
@@ -21,7 +21,7 @@ describe 'Car' do
       expect(car.color).to eq('foo')
     end
 
-    it 'allows reading for :wheels' do
+    it 'allows reading for :wheels' do # only attribute reader set on the class
       car = Car.new
       expect(car.wheels).to eq(4)
     end
@@ -29,10 +29,24 @@ describe 'Car' do
 
   describe '.colors' do
 
-
+    it 'returns an array of color names' do
+      c = ['blue', 'black', 'red', 'green']
+      expect(Car.colors).to match_array(c) # Class method
+    end
   end
 
   describe '#full_name' do
 
+    if 'returns a string in the expected format' do
+      @honda = Car.new(make: 'Honda', year: 2004, color: 'blue')
+      expect(@honda.full_name).to eq ('2004 Honda (blue)')
+    end
+
+    context 'when initialized with no arguments' do # so, 'in this context' then
+      it 'returns a string using default values' do
+        car = Car.new
+        expect(car.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
   end
 end
