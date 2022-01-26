4c8854af1cf13e9060e39c524d1d32bbf1560b03
matias acuna <agroang@gmail.com>
2022-01-26 15:58:24 +0900

    wrote tests for attributes

--- testing-study/basic-rspec/spec/car_spec.rb
+++ testing-study/basic-rspec/spec/car_spec.rb
@@ -3,9 +3,32 @@ require 'car'
 describe 'Car' do
   describe 'attributes' do
 
+    it 'allows reading and writing for :make' do
+      car = Car.new
+      car.make = 'Test' # writing here
+      expect(car.make).to eq('Test') # reading here
+    end
+
+    it 'allows reading and writing for :year' do
+      car = Car.new
+      car.year = 9999
+      expect(car.year)to. eq(9999)
+    end
+
+    it 'allows reading and writing for :color' do
+      car = Car.new
+      car.color = 'foo'
+      expect(car.color).to eq('foo')
+    end
+
+    it 'allows reading for :wheels' do
+      car = Car.new
+      expect(car.wheels).to eq(4)
+    end
   end
 
   describe '.colors' do
+
 
   end
 
