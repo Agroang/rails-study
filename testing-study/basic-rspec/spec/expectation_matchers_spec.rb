0647ce5d07c15340d7b690aa87b7566b3fb78dbf
matias acuna <agroang@gmail.com>
2022-01-27 17:48:50 +0900

    added compound expectations

--- testing-study/basic-rspec/spec/expectation_matchers_spec.rb
+++ testing-study/basic-rspec/spec/expectation_matchers_spec.rb
@@ -324,4 +324,18 @@ describe 'Expectation Matchers' do
       expect { warn('problem') }.to output(/problem/).to_stderr
     end
   end
+
+  describe 'compound expectations' do
+
+    it 'will match using: and, or, &, |' do
+      expect([1, 2, 3, 4]).to start_with(1).and end_with(4)
+
+      expect([1, 2, 3, 4]).to start_with(1) & include(2)
+
+      expect(10 * 10).to be_odd.or be > 50
+
+      array = ['hello', 'goodbye'].shuffle
+      expect(array.first).to eq('hello') | eq('goodbye')
+    end
+  end
 end
