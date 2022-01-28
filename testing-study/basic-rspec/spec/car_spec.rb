c5eca33a292e90b79e47de2839559fcfb3f2d1ba
matias acuna <agroang@gmail.com>
2022-01-28 11:01:12 +0900

    added before hooks to the car_spec

--- testing-study/basic-rspec/spec/car_spec.rb
+++ testing-study/basic-rspec/spec/car_spec.rb
@@ -1,29 +1,40 @@
 require 'car'
 
 describe 'Car' do
+
   describe 'attributes' do
 
+    # adding a before hook for examples, in this case we use example and not
+    # describe because we are changing values and those would remain for
+    # describe/context as it would be using the same variable, commenting
+    # out the old variable just to remember what we did before.
+
+    before(:example) do
+      # we must use an instance variable to be available to the examples
+      @car = Car.new
+    end
+
     it 'allows reading and writing for :make' do
-      car = Car.new
-      car.make = 'Test' # writing here
-      expect(car.make).to eq('Test') # reading here
+      # car = Car.new
+      @car.make = 'Test' # writing here
+      expect(@car.make).to eq('Test') # reading here
     end
 
     it 'allows reading and writing for :year' do
-      car = Car.new
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      # car = Car.new
+      @car.year = 9999
+      expect(@car.year).to eq(9999)
     end
 
     it 'allows reading and writing for :color' do
-      car = Car.new
-      car.color = 'foo'
-      expect(car.color).to eq('foo')
+      # car = Car.new
+      @car.color = 'foo'
+      expect(@car.color).to eq('foo')
     end
 
     it 'allows reading for :wheels' do # only attribute reader set on the class
-      car = Car.new
-      expect(car.wheels).to eq(4)
+      # car = Car.new
+      expect(@car.wheels).to eq(4)
     end
 
     it 'allows writing for :doors' # without the do/end it will become 'pending'
