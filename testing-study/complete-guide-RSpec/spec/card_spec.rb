7533211266978368466ce3cd47536e4a5727e153
matias acuna <agroang@gmail.com>
2022-02-17 16:31:31 +0900

    added small memo about writing RSpec.describe

--- testing-study/complete-guide-RSpec/spec/card_spec.rb
+++ testing-study/complete-guide-RSpec/spec/card_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Card' do
+RSpec.describe 'Card' do # no need to use RSpec.describe, describe suffice
   it 'has a type' do
     card = Card.new('Ace of Spades')
     expect(card.type).to eq('Ace of Spades')
