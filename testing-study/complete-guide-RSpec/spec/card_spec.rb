4c4e1b4eebad635632337e7117313b4064cac47a
matias acuna <agroang@gmail.com>
2022-02-17 17:41:22 +0900

    added basic class to make the spec pass

--- testing-study/complete-guide-RSpec/spec/card_spec.rb
+++ testing-study/complete-guide-RSpec/spec/card_spec.rb
@@ -1,3 +1,11 @@
+class Card
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+end
+
 RSpec.describe 'Card' do # no need to use RSpec.describe, describe suffice
   it 'has a type' do
     card = Card.new('Ace of Spades')
