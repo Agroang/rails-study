458f34404990748e76512e6d2a047c10c3b31bf8
matias acuna <agroang@gmail.com>
2022-02-18 10:35:07 +0900

    added some comments about the limitations of the helper method

--- testing-study/complete-guide-RSpec/spec/card_spec.rb
+++ testing-study/complete-guide-RSpec/spec/card_spec.rb
@@ -17,12 +17,16 @@ RSpec.describe 'Card' do # no need to use RSpec.describe, describe suffice
   def card # helper method that evaluates to a new card, using it bellow
     Card.new('Ace', 'Spade')
   end
+  # there is a big problem with the above method. It does not allow mutation,
+  # if you try to assign a different value within the example you will end
+  # up just calling the method again, and it will always have the same values.
 
   it 'has a rank' do
     # card = Card.new('Ace', 'Spade') no longer needed because we have the
     # before hook creating the card
     # expect(@card.rank).to eq('Ace')
     expect(card.rank).to eq('Ace') # calling card method
+    # card.rank = 'Queen' won't work, you are just calling the method again
   end
 
   it 'has a suit' do
