621a8af61e14a280c194914c6f5877602ed4599b
matias acuna <agroang@gmail.com>
2022-02-18 10:26:28 +0900

    instead of before hook using a method helper to avoid using instance variables

--- testing-study/complete-guide-RSpec/spec/card_spec.rb
+++ testing-study/complete-guide-RSpec/spec/card_spec.rb
@@ -9,19 +9,25 @@ end
 
 RSpec.describe 'Card' do # no need to use RSpec.describe, describe suffice
 
-  before do
-  # before(:example) do ... this is also another way, you can put :context,
-  # :suit, more info later and/or in my other notes of linkedin tutorial
-    @card = Card.new('Ace', 'Spade')
+  # before do
+  # # before(:example) do ... this is also another way, you can put :context,
+  # # :suit, more info later and/or in my other notes of linkedin tutorial
+  #   @card = Card.new('Ace', 'Spade')
+  # end
+  def card # helper method that evaluates to a new card, using it bellow
+    Card.new('Ace', 'Spade')
   end
+
   it 'has a rank' do
     # card = Card.new('Ace', 'Spade') no longer needed because we have the
     # before hook creating the card
-    expect(@card.rank).to eq('Ace')
+    # expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace') # calling card method
   end
 
   it 'has a suit' do
     # card = Card.new('Ace', 'Spade')
-    expect(@card.suit).to eq('Spade')
+    # expect(@card.suit).to eq('Spade')
+    expect(card.suit).to eq('Spade')
   end
 end
