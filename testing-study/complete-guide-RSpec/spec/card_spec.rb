45938ea5893651b3ac4743e5715141209b6f9200
matias acuna <agroang@gmail.com>
2022-02-18 10:11:06 +0900

    added a before hook creating a car instance variable

--- testing-study/complete-guide-RSpec/spec/card_spec.rb
+++ testing-study/complete-guide-RSpec/spec/card_spec.rb
@@ -8,13 +8,20 @@ class Card
 end
 
 RSpec.describe 'Card' do # no need to use RSpec.describe, describe suffice
+
+  before do
+  # before(:example) do ... this is also another way, you can put :context,
+  # :suit, more info later and/or in my other notes of linkedin tutorial
+    @card = Card.new('Ace', 'Spade')
+  end
   it 'has a rank' do
-    card = Card.new('Ace', 'Spade')
-    expect(card.rank).to eq('Ace')
+    # card = Card.new('Ace', 'Spade') no longer needed because we have the
+    # before hook creating the card
+    expect(@card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    card = Card.new('Ace', 'Spade')
-    expect(card.suit).to eq('Spade')
+    # card = Card.new('Ace', 'Spade')
+    expect(@card.suit).to eq('Spade')
   end
 end
