91c2031d65c0e0951ce2baf010989e8461d8154c
matias acuna <agroang@gmail.com>
2022-02-18 09:45:49 +0900

    refactored and added another example, still not DRY but will refactor soon

--- testing-study/complete-guide-RSpec/spec/card_spec.rb
+++ testing-study/complete-guide-RSpec/spec/card_spec.rb
@@ -7,8 +7,13 @@ class Card
 end
 
 RSpec.describe 'Card' do # no need to use RSpec.describe, describe suffice
-  it 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+  it 'has a rank' do
+    card = Card.new('Ace', 'Spade')
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    card = Card.new('Ace', 'Spade')
+    expect(card.suit).to eq('Spade')
   end
 end
