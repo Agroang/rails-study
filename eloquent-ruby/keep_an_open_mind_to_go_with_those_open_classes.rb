6428fbc1ddf1feec92219d06c76c24e271fca0e3
matias acuna <agroang@gmail.com>
2022-04-14 13:54:46 +0900

    Finished last chapter that is just a small conclusion

--- eloquent-ruby/keep_an_open_mind_to_go_with_those_open_classes.rb
+++ eloquent-ruby/keep_an_open_mind_to_go_with_those_open_classes.rb
@@ -1,1 +1,3 @@
-# Throughout the writing of this book I have been haunted by a feeling of irony.
+# Ruby is a very, very sharp programming tool, and it pays to know what you are
+# doing before you pick it up.
+# The rules are not set in stone, these are recommendations.
