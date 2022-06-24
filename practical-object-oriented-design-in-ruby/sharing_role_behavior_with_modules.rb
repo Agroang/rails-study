02231893c7b44bc6c0dc6a105e3aae4dcd5fbfce
matias acuna <agroang@gmail.com>
2022-06-24 17:25:04 +0900

    RIP (Read in Progress)

---
+++ practical-object-oriented-design-in-ruby/sharing_role_behavior_with_modules.rb
@@ -0,0 +1,118 @@
+# To reap benefits from using inheritance you must understand not only how to
+# write inheritable code but also when it makes sense to do so. Use of classical
+# inheritance is always optional; every problem that it solves can be solved
+# another way. Because no design technique is free, creating the most
+# cost-effective application requires making informed tradeoffs between the
+# relative costs and likely benefits of alternatives.
+# This chapter explores an alternative that uses the techniques of inheritance
+# to share a role.
+
+# Some problems require sharing behavior among otherwise unrelated objects. This
+# common behavior is orthogonal to class; it’s a role an object plays.
+
+# When a role needs shared behavior, you’re faced with the problem of organizing
+# the shared code. Ideally this code would be defined in a single place but be
+# usable by any object that wished to act as the duck type and play the role.
+
+# Methods can be defined in a module and then the module can be added to any
+# object. Modules thus provide a perfect way to allow objects of different
+# classes to play a common role using a single set of code.
+
+# When an object includes a module, the methods defined therein become available
+# via automatic delegation. If this sounds like classical inheritance, it also
+# looks like it, at least from the point of view of the including object. From
+# that object’s point of view, messages arrive, it doesn’t understand them, they
+# get automatically routed somewhere else, the correct method implementation is
+# magically found, it is executed, and the response is returned.
+
+# The total set of messages to which an object can respond includes:
+# • Those it implements
+# • Those implemented in all objects above it in the hierarchy
+# • Those implemented in any module that has been added to it
+# • Those implemented in all modules added to any object above it in the
+#   hierarchy
+
+# It’s easiest to illustrate these dependencies with an extreme example. Imagine
+# a StringUtils class that implements utility methods for managing strings.
+# You can ask StringUtils if a string is empty by sending StringUtils.
+# empty?(some_string).
+# If you have written much object-oriented code you will find this idea
+# ridiculous. Using a separate class to manage strings is patently redundant;
+# strings are objects, they have their own behavior, they manage themselves.
+# Requiring that other objects know about a third party, StringUtils, to get
+# behavior from a string complicates the code by adding an unnecessary
+# dependency.
+
+# The rules for modules are the same as for classical inheritance. If a module
+# sends a message, it must provide an implementation, even if that
+# implementation merely raises an error indicating that users of the module must
+# implement the method.
+
+# This may not fit the strict definition of classical inheritance, but in terms
+# of how the code should be written and how the messages are resolved, it
+# certainly acts like it. The coding techniques are the same because method
+# lookup follows the same path.
+
+# Classic inheritance versus modules is like a "is-a" versus "behaves-like-a"
+# difference definitely matters, and each choice has distinct consequences.
+# However, the coding techniques for these two things are very similar and this
+# similarity exists because both techniques rely on automatic message delegation.
+
+# When Bicycle includes Schedulable, all of the methods defined in the module
+# become part of Bicycle’s response set. The module’s methods go into the method
+# lookup path directly above methods defined in Bicycle. Including this module
+# doesn’t change Bicycle’s superclass (that’s still Object), but as far as
+# method lookup is concerned, it may as well have. Any message received by an
+# instance of MountainBike now stands a chance of being satisfied by a method
+# defined in the Schedulable module.
+# This has enormous implications. If Bicycle implements a method that is also
+# defined in Schedulable, Bicycle’s implementation overrides Schedulable’s.
+
+# When a single class includes several different modules, the modules are placed
+# in the method lookup path in reverse order of module inclusion. Thus, the
+# methods of the last included module are encountered first in the lookup path.
+
+# This discussion has, until now, been about including modules into classes via
+# Ruby’s include keyword. As you have already seen, including a module into a
+# class adds the module’s methods to the response set for all instances of that
+# class. For example, the Schedulable module was included into the Bicycle class,
+# and, as a result, instances of MountainBike gain access to the methods defined
+# therein.
+# However, it is also possible to add a module’s methods to a single object,
+# using Ruby’s extend keyword. Because extend adds the module’s behavior directly
+# to an object, extending a class with a module creates class methods in that
+# class and extending an instance of a class with a module creates instance
+# methods in that instance. These two things are exactly the same; classes are,
+# after all, just plain old objects, and extend behaves the same for all.
+
+# Finally, any object can also have ad hoc methods added directly to its own
+# personal “Singleton class.” These ad hoc methods are unique to this specific
+# object.
+
+# The usefulness and maintainability of inheritance hierarchies and modules is
+# in direct proportion to the quality of the code.
+
+# There are two antipatterns that indicate that your code might benefit from
+# inheritance:
+# First, an object that uses a variable with a name like type or category to
+# determine what message to send to self contains two highly related but
+# slightly different types.
+# Second, when a sending object checks the class of a receiving object to
+# determine what message to send, you have overlooked a duck type. This is
+# another maintenance nightmare; the code must change every time you introduce a
+# new class of receiver.
+
+# All of the code in an abstract superclass should apply to every class that
+# inherits it. Superclasses should not contain code that applies to some, but
+# not all, subclasses. This restriction also applies to modules: The code in a
+# module must apply to all who use it.
+
+# Subclasses that override a method to raise an exception like “does not
+# implement” are a symptom of this problem. While it is true that expediency
+# pays for all and that it is sometimes most cost effective to arrange code in
+# just this way, you should be reluctant to do so. When subclasses override a
+# method to declare that they do not do that thing, they come perilously close
+# to declaring that they are not that thing. Nothing good can come of this.
+# If you cannot correctly identify the abstraction, there may not be one, and
+# if no common abstraction exists, then inheritance is not the solution to your
+# design problem.
